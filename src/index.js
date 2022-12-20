b30db679a2b66967b93d09fe0d0e9137ad02d650
Nguyenminhhoang952000 <codon1visao952000@gmail.com>
2022-12-20 15:06:52 +0700

    Bootstrap 4

--- src/index.js
+++ src/index.js
@@ -4,19 +4,25 @@ const morgan = require('morgan');
 const handlebars = require('express-handlebars');
 const app = express();
 const port = 3000;
+
+app.use(express.static(path.join(__dirname,'public')))
 // HTTP require
 app.use(morgan('combined'));
 
 //Template engine
-app.engine('handlebars', handlebars());
-app.set('view engine', 'handlebars');
+app.engine('hbs', handlebars.engine({
+  extname:'hbs'
+}));
+app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
-console.log(__dirname);
 
+//Routes
 app.get('/', (req, res) => {
   res.render('home');
 })
-
+app.get('/news', (req, res) => {
+  res.render('news');
+})
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
